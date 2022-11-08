44a42ef978375dbeeb16f087835418968617f105
Ryan Zimmerman <zimmerry@oregonstate.edu>
2022-11-07 19:03:43 -0800

    Update app.js

--- scc-web/src/App.js
+++ scc-web/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sensor-Based Climate Control FrontEnd
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/sensor-climate-control"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github Org
         </a>
       </header>
     </div>
