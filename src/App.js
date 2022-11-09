be5f5b54d91d72029009debd1a4ec53e490162c8
Ryan Zimmerman <zimmerry@oregonstate.edu>
2022-11-09 15:31:04 -0800

    Add bootstrap, header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './features/application/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sensor-Based Climate Control FrontEnd
+          Sensor-Based Climate Control Frontend
         </p>
         <a
           className="App-link"
