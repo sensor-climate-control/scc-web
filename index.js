03a8fa27f81cab8258016eb35ef631fa65ddc6b3
Ryan Zimmerman <zimmerry@oregonstate.edu>
2022-11-18 12:42:48 -0800

    Add tar steps to GH action workflow, change to port 80 default

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const api = require('./server/api')
 
 const hostname = '127.0.0.1';
-const port = 3001;
+const port = process.env.PORT || 80;
 
 var app = express();
 
