586a73153a6fe4869f4709ea9fed2c483f002d8f
Daniel Piper <piperdaniel1@gmail.com>
2022-11-18 18:17:02 -0800

    Temp/AQI displays on card

--- src/features/weather/CurrentWeather.js
+++ src/features/weather/CurrentWeather.js
@@ -1,10 +1,61 @@
 import MyCard from "../application/MyCard";
+import { useEffect, useState } from "react";
+import { endpoint } from "../../App";
 
 const CurrentWeather = () => {
+    const [temp, setTemp] = useState(0);
+    const [humidity, setHumidity] = useState(0);
+    const [feelsLike, setFeelsLike] = useState(0);
+
+    /*
+     * Query the AQI from the backend server
+     */
+    async function getTempStats() {
+        return new Promise(async (resolve, _) => {
+            const res = await fetch(endpoint + "/weather/now");
+            const data = await res.json();
+
+            const temp = data.main.temp;
+            const humidity = data.main.humidity;
+            const feelsLike = data.main.feels_like;
+
+            resolve({ temp, humidity, feelsLike });
+        });
+    }
+
+    /*
+     * Updates the AQI and AQI color every ten seconds once the component is mounted.
+     * Clears the timer when the component is unmounted.
+     */
+    useEffect(() => {
+        const color_map = {
+            1: "#00ff88",
+            2: "#ffff00",
+            3: "#ff8800",
+            4: "#ff0000",
+            5: "#99004c",
+            6: "#7e0023",
+        }
+
+        function updateTemp() {
+            getTempStats().then(({ temp, humidity, feelsLike }) => {
+                setTemp(temp);
+                setHumidity(humidity);
+                setFeelsLike(feelsLike);
+            });
+        }
+
+        updateTemp();
+        const interval = setInterval(updateTemp, 1000 * 10);
+
+        return () => clearInterval(interval);
+    });
     return (
         <MyCard title="Current Weather">
-            Temp: 57°F
-            Humidity: 25%
+            <p>Temperature: {temp}°F</p>
+            <p>Feels Like: {feelsLike}°F</p>
+            <p>Humidity: {humidity}%</p>
+
         </MyCard>
     )
 }
