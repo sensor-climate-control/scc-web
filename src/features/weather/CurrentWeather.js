59a1d9b12323c448f169b4aad8ea908e560ad067
Daniel Piper <piperdaniel1@gmail.com>
2022-12-15 11:19:07 -0800

    weatherSlice redux

--- src/features/weather/CurrentWeather.js
+++ src/features/weather/CurrentWeather.js
@@ -1,56 +1,11 @@
 import MyCard from "../application/MyCard";
-import { useEffect, useState } from "react";
-import { endpoint } from "../../App";
+import { useSelector } from 'react-redux';
 
 const CurrentWeather = () => {
-    const [temp, setTemp] = useState(0);
-    const [humidity, setHumidity] = useState(0);
-    const [feelsLike, setFeelsLike] = useState(0);
+    const temp = useSelector(state => state.weather.value.temp);
+    const humidity = useSelector(state => state.weather.value.humidity);
+    const feelsLike = useSelector(state => state.weather.value.feelsLike);
 
-    /*
-     * Query the AQI from the backend server
-     */
-    async function getTempStats() {
-        return new Promise(async (resolve, _) => {
-            const res = await fetch(endpoint + "weather/now");
-            const data = await res.json();
-
-            const temp = data.main.temp;
-            const humidity = data.main.humidity;
-            const feelsLike = data.main.feels_like;
-
-            resolve({ temp, humidity, feelsLike });
-        });
-    }
-
-    /*
-     * Updates the AQI and AQI color every ten seconds once the component is mounted.
-     * Clears the timer when the component is unmounted.
-     */
-    useEffect(() => {
-        /* commented out to appease ESLint */
-        // const color_map = {
-        //     1: "#00ff88",
-        //     2: "#ffff00",
-        //     3: "#ff8800",
-        //     4: "#ff0000",
-        //     5: "#99004c",
-        //     6: "#7e0023",
-        // }
-
-        function updateTemp() {
-            getTempStats().then(({ temp, humidity, feelsLike }) => {
-                setTemp(temp);
-                setHumidity(humidity);
-                setFeelsLike(feelsLike);
-            });
-        }
-
-        updateTemp();
-        const interval = setInterval(updateTemp, 1000 * 300); // update every 5 minutes
-
-        return () => clearInterval(interval);
-    });
     return (
         <MyCard title="Current Weather">
             <p>Temperature: {temp}°F</p>
