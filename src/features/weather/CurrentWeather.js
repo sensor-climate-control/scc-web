3a9df92a413d80f5e8a8e65049d6455c667195b2
Daniel Piper <piperdaniel1@gmail.com>
2022-12-15 17:32:38 -0800

    trying to refresh data through redux toolkit

--- src/features/weather/CurrentWeather.js
+++ src/features/weather/CurrentWeather.js
@@ -1,10 +1,14 @@
 import MyCard from "../application/MyCard";
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { refreshData } from '../redux/weatherSlice';
 
 const CurrentWeather = () => {
     const temp = useSelector(state => state.weather.value.temp);
     const humidity = useSelector(state => state.weather.value.humidity);
     const feelsLike = useSelector(state => state.weather.value.feelsLike);
+
+    const dispatch = useDispatch();
+    dispatch(refreshData());
 
     return (
         <MyCard title="Current Weather">
