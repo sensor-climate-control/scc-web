f2d9b072ae6c2b5a0bc7a0604596d239eb3575c3
Daniel Piper <piperdaniel1@gmail.com>
2022-12-15 17:32:38 -0800

    trying to refresh data through redux toolkit

--- src/features/redux/weatherSlice.js
+++ src/features/redux/weatherSlice.js
@@ -1,25 +1,31 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { endpoint } from '../../App'
 
 const initialState = {
     value: {temp: 0, humidity: 0, feelsLike: 0},
 }
 
 export const weatherSlice = createSlice({
-  name: 'weather',
-  initialState,
-  reducers: {
-    setTemp: (state, action) => {
-      state.value.temp = action.payload;
+    name: 'weather',
+    initialState,
+    reducers: {
+        setTemp: (state, action) => {
+            state.value.temp = action.payload;
+        },
+        setHumidity: (state, action) => {
+            state.value.humidity = action.payload;
+        },
+        setFeelsLike: (state, action) => {
+            state.value.feelsLike = action.payload;
+        },
+        refreshData: async (state, action) => {
+            let res = await fetch(endpoint + '/now')
+
+            console.log(res.body)
+        }
     },
-    setHumidity: (state, action) => {
-      state.value.humidity = action.payload;
-    },
-    setFeelsLike: (state, action) => {
-      state.value.feelsLike = action.payload;
-    }
-  },
 })
 
-export const { setWeather } = weatherSlice.actions;
+export const { setTemp, setHumidity, setFeelsLike, refreshData } = weatherSlice.actions;
 
 export default weatherSlice.reducer;
