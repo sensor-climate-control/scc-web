9b9e26b11a5b69c8f5fa9106fe01e39108f691bb
Ryan Zimmerman <zimmerry@oregonstate.edu>
2023-01-13 11:39:34 -0800

    Work on api endpoints for sensor operations

--- server/api/homes.js
+++ server/api/homes.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
-const homes = require('../data/homes.json')
-const sensors = require('../data/sensors.json');
 const { validateAgainstSchema, extractValidFields } = require('../lib/validation');
 const { HomeSchema, insertNewHome, getAllHomes, getHomeById, deleteHomeById, updateHomeById } = require('../models/homes');
+const { SensorSchema, getSensorById, insertNewSensor, updateSensorById, deleteSensorById, SensorReadingSchema } = require('../models/sensors');
 
 // Creates new home
 // Requires authentication
@@ -17,7 +16,7 @@ router.post('/', async function (req, res, next) {
         } catch (err) {
             console.error(err)
             res.status(500).send({
-                error: "Error inserting business into DB. Please try again later."
+                error: "Error inserting home into DB. Please try again later."
             })
         }
     } else {
@@ -51,10 +50,9 @@ router.get('/:homeid', async function (req, res, next) {
 router.put('/:homeid', async function (req, res, next) {
     const homeid = req.params.homeid
     if (validateAgainstSchema(req.body, HomeSchema)) {
-        console.log("=== good input")
         const home = extractValidFields(req.body, HomeSchema)
         const successfulUpdate = await updateHomeById(homeid, home)
-        console.log("==== successfulUpdate: ", successfulUpdate)
+        
         if (successfulUpdate) {
             res.status(200).json({
                 links: {
@@ -86,48 +84,127 @@ router.delete('/:homeid', async function (req, res, next) {
 // Fetches all sensors for home
 // Requires admin or home member auth
 router.get('/:homeid/sensors', async function (req, res, next) {
-    res.status(200).send(homes[0].sensors)
+    const homeid = req.params.homeid
+    const home = await getHomeById(homeid)
+    if (home) {
+        res.status(200).send(home.sensors)
+    } else {
+        next();
+    }
 })
 
 // Adds new sensor to home
 // Requires admin or home admin auth
 router.post('/:homeid/sensors', async function (req, res, next) {
-    res.status(201).send({
-        sensorid: "sensor1234"
-    })
+    console.log("==== sensor: ", req.body)
+    if (validateAgainstSchema(req.body, SensorSchema)) {
+        try {
+            const id = await insertNewSensor(req.body)
+            res.status(201).send({
+                id: id
+            })
+        } catch (err) {
+            console.error(err)
+            res.status(500).send({
+                error: "Error inserting sensor into DB. Please try again later."
+            })
+        }
+    } else {
+        res.status(400).send({
+            error: "Request body does not contain a valid sensor object"
+        })
+    }
 })
 
 // Fetches info for single sensor
 // Requires admin or home member auth
 router.get('/:homeid/sensors/:sensorid', async function (req, res, next) {
-    res.status(200).send(sensors[0])
+    const sensorid = req.params.sensorid
+    const sensor = await getSensorById(sensorid)
+    if (sensor) {
+        res.status(200).send(sensor)
+    } else {
+        next();
+    }
 })
 
 // Updates info for single sensor
 // Requires admin or home admin auth
 router.put('/:homeid/sensors/:sensorid', async function (req, res, next) {
-    res.status(200).send(sensors[0])
+    const homeid = req.params.homeid
+    const sensorid = req.params.sensorid
+    if (validateAgainstSchema(req.body, SensorSchema)) {
+            const sensor = extractValidFields(req.body, SensorSchema)
+            const successfulUpdate = await updateSensorById(sensorid, sensor)
+            
+            if (successfulUpdate) {
+                res.status(200).json({
+                    links: {
+                        home: `/api/homes/${homeid}/sensors/${sensorid}`
+                    }
+                });
+            } else {
+                next();
+            }
+    } else {
+        res.status(400).send({
+            error: "Request body does not contain a valid sensor object"
+        })
+    }
 })
 
 // Deletes sensor
 // Requires admin or home admin auth
 router.delete('/:homeid/sensors/:sensorid', async function (req, res, next) {
-    res.status(204).send()
+    const sensorid = req.params.sensorid
+    const homeid = req.params.homeid
+    const successfulDeletion = await deleteSensorById(sensorid, homeid)
+    if (successfulDeletion) {
+        res.status(204).send();
+    } else {
+        next();
+    }
 })
 
 // Fetches readings from single sensor
 // Requires admin or home member auth
 router.get('/:homeid/sensors/:sensorid/readings', async function (req, res, next) {
-    res.status(200).send(sensors[0].readings)
+    const sensorid = req.params.sensorid
+    const sensor = await getSensorById(sensorid)
+    if (sensor) {
+        res.status(200).send(sensor.readings)
+    } else {
+        next();
+    }
 })
 
 // Adds readings from single sensor
 // Requires admin or home member auth
 // Optional: API key
-router.post('/:homeid/sensors/:sensorid/readings', async function (req, res, next) {
-    res.status(200).send({
-        "readings_added": sensors[0].readings
-    })
+router.put('/:homeid/sensors/:sensorid/readings', async function (req, res, next) {
+    const sensorid = req.params.sensorid
+    const homeid = req.params.homeid
+    const readings = req.body
+    const validReadings = readings.filter(reading => validateAgainstSchema(reading, SensorReadingSchema))
+    if (validReadings.length > 0) {
+        let sensor = await getSensorById(sensorid)
+        sensor.readings.push(validReadings)
+
+        const successfulUpdate = await updateSensorById(sensorid, sensor)
+        if (successfulUpdate) {
+            res.status(200).json({
+                links: {
+                    readings: `/api/homes/${homeid}/sensors/${sensorid}/readings`
+                }
+            });
+        } else {
+            next();
+        }
+    } else {
+        res.status(400).json({
+            error: "Request body does not contain an array of valid sensor reading objects"
+        })
+    }
 })
 
 exports.router = router;
---
+++ server/models/sensors.js
@@ -0,0 +1,76 @@
+const { ObjectId } = require('mongodb')
+
+const { extractValidFields } = require('../lib/validation')
+const { getDbReference } = require('../lib/mongo')
+const { getHomeById, updateHomeById } = require('./homes')
+
+const SensorSchema = {
+    name: { required: true },
+    active: {default: true },
+    home: { required: true },
+    location: { required: true },
+    readings: { default: [] }
+}
+exports.SensorSchema = SensorSchema
+
+const SensorReadingSchema = {
+    date_time: { required: true },
+    temp: { required: true },
+    humidity: { required: true }
+}
+exports.SensorReadingSchema = SensorReadingSchema
+
+async function insertNewSensor(sensor) {
+    sensor = extractValidFields(sensor, SensorSchema)
+    const db = getDbReference()
+    const collection = db.collection('sensors')
+    const result = await collection.insertOne(sensor)
+
+    let home = await getHomeById(sensor.home)
+    home.sensors.push(result.insertedId)
+    await updateHomeById(sensor.home, home)
+
+    return result.insertedId
+}
+exports.insertNewSensor = insertNewSensor
+
+async function getSensorById(id) {
+    const db = getDbReference()
+    const collection = db.collection('sensors')
+    const sensor = await collection.find({
+        _id: id
+    }).toArray()
+
+   return sensor[0]
+}
+exports.getSensorById = getSensorById
+
+async function updateSensorById(id, sensor) {
+    sensor = extractValidFields(sensor, SensorSchema)
+    const db = getDbReference()
+    const collection = db.collection('sensors')
+    const result = await collection.replaceOne(
+        { _id: new ObjectId(id) },
+        sensor
+    )
+
+    console.log("==== result: ", result)
+    return result.matchedCount > 0;
+}
+exports.updateSensorById = updateSensorById
+
+async function deleteSensorById(id, homeid) {
+    const db = getDbReference()
+    const collection = db.collection('sensors')
+    const result = await collection.deleteOne({
+        _id: id
+    })
+
+    let home = await getHomeById(homeid)
+    home.sensors = home.sensors.filter(sensor => sensor !== id)
+    await updateHomeById(homeid, home)
+
+    return result.deletedCount > 0;
+}
+exports.deleteSensorById = deleteSensorById
+
