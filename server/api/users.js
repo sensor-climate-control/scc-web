c693116308c8b2fcd0ad9505cd0fc994ef0d599b
Ryan Zimmerman <zimmerry@oregonstate.edu>
2023-01-23 16:38:40 -0800

    Setup Users API endpoints

--- server/api/users.js
+++ server/api/users.js
@@ -1,47 +1,91 @@
 const router = require('express').Router();
-const users = require('../data/users.json')
+// const users = require('../data/users.json')
+const { validateAgainstSchema, extractValidFields } = require('../lib/validation');
+const { UserSchema, insertNewUser, getAllUsers, getUserById, updateUserById, deleteUserById } = require('../models/users')
 
 // Creates new user
 router.post('/', async function (req, res, next) {
-    res.status(201).send({
-        userid: users[0].name
-    });
+    if (validateAgainstSchema(req.body, UserSchema)) {
+        try {
+            const id = await insertNewUser(req.body)
+            console.log("==== new user id: ", id)
+            res.status(201).send({
+                id: id
+            })
+        } catch (err) {
+            console.error(err)
+            res.status(500).send({
+                error: "Error inserting user into DB. Please try again later."
+            })
+        }
+    } else {
+        res.status(400).send({
+            error: "Request body does not contain a valid user object"
+        })
+    }
 });
 
 // Returns all the users
 // Requres admin auth
 router.get('/', async function (req, res, next) {
-    res.status(200).send({
-        users: users
-    });
+    const allUsers = await getAllUsers()
+    res.status(200).send(allUsers)
 });
 
 // Returns requested user details
 // Requires auth from requested user or admin
 router.get('/:userid', async function (req, res, next) {
-    res.status(200).send(users[0])
+    const userid = req.params.userid
+    const user = await getUserById(userid)
+    if (user) {
+        res.status(200).send(user)
+    } else {
+        next();
+    }
 });
 
 // Update user details
 // Requires auth from requested user or admin
 router.put('/:userid', async function (req, res, next) {
-    res.status(201).send({
-        userid: users[0].name
-    });
+    const userid = req.params.userid
+    if (validateAgainstSchema(req.body, UserSchema)) {
+        const user = extractValidFields(req.body, UserSchema)
+        const successfulUpdate = await updateUserById(userid, user)
+        
+        if (successfulUpdate) {
+            res.status(200).json({
+                links: {
+                    user: `/api/users/${userid}`
+                }
+            });
+        } else {
+            next();
+        }
+    } else {
+        res.status(400).json({
+            error: "Request body is not a valid user object"
+        })
+    }
 });
 
 // Deletes user
 // Requires admin auth
 router.delete('/:userid', async function (req, res, next) {
-    res.status(204).end();
+    const userid = req.params.userid
+    const successfulDeletion = await deleteUserById(userid)
+    if (successfulDeletion) {
+        res.status(204).send();
+    } else {
+        next();
+    }
 });
 
-// Returns login token if username/email and password match
-router.post('/login', async function (req, res) {
-    res.status(200).send({
-        userid: "user123",
-        token: "token123"
-    });
-});
+// // Returns login token if username/email and password match
+// router.post('/login', async function (req, res) {
+//     res.status(200).send({
+//         userid: "user123",
+//         token: "token123"
+//     });
+// });
 
 exports.router = router;
--- server/models/users.js
+++ server/models/users.js
@@ -1,13 +1,11 @@
-/* commented out to appease ESLint */
-// const { ObjectId } = require('mongodb')
+const { ObjectId } = require('mongodb')
 
-// const { extractValidFields } = require('../lib/validation')
-// const { getDbReference } = require('../lib/mongo')
+const { extractValidFields } = require('../lib/validation')
+const { getDbReference } = require('../lib/mongo')
 
 const UserSchema = {
     name: { required: true },
     email: { required: true },
-    password: { required: true },
     admin: { default: false },
     phone: { required: false },
     phone_carrier: { required: false },
@@ -15,3 +13,57 @@ const UserSchema = {
     homes: { default: [] }
 }
 exports.UserSchema = UserSchema
+
+async function insertNewUser(user) {
+    user = extractValidFields(user, UserSchema)
+    const db = getDbReference()
+    const collection = db.collection('users')
+    const result = await collection.insertOne(user)
+    return result.insertedId
+}
+exports.insertNewUser = insertNewUser
+
+async function getAllUsers() {
+    const db = getDbReference()
+    const collection = db.collection('users')
+    const users = await collection.find({}).toArray()
+    console.log("==== users: ", users)
+    return users
+}
+exports.getAllUsers = getAllUsers
+
+async function getUserById(id) {
+    const db = getDbReference()
+    const collection = db.collection('users')
+    const user = await collection.find({
+        _id: id
+    }).toArray()
+
+    console.log("==== user: ", user)
+
+    return user[0]
+}
+exports.getUserById = getUserById
+
+async function updateUserById(id, user) {
+    user = extractValidFields(user, UserSchema)
+    const db = getDbReference()
+    const collection = db.collection('users')
+    const result = await collection.replaceOne(
+        { _id: new ObjectId(id) },
+        user
+    )
+    console.log("==== result: ", result)
+    return result.matchedCount > 0;
+}
+exports.updateUserById = updateUserById
+
+async function deleteUserById(id) {
+    const db = getDbReference()
+    const collection = db.collection('users')
+    const result = await collection.deleteOne({
+        _id: id
+    })
+    return result.deletedCount > 0;
+}
+exports.deleteUserById = deleteUserById
