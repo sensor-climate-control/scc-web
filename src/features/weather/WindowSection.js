6d545d5fa9561f06275e744608d0e6051a91444a
Daniel Piper <piperdaniel1@gmail.com>
2023-01-23 16:04:46 -0800

    running lights for window status

--- src/features/weather/WindowSection.js
+++ src/features/weather/WindowSection.js
@@ -1,11 +1,20 @@
 import "./WindowSection.css"
 
 export default function WindowSection(props) {
+    let bg_color = "";
+    if (props.status == "open") {
+        bg_color = "#4aff71";
+    } else if (props.status == "closed") {
+        bg_color = "#ff0000";
+    } else {
+        bg_color = "#efff00";
+    }
+
     return (
         <button className="window-section-wrapper">
             <div className="inner-window-section-wrapper">
                 <div className="window-section-indicator">
-                    <div className="window-section-indicator-circle" />
+                    <div className="window-section-indicator-circle" style={{backgroundColor: bg_color}}/>
                 </div>
 
                 <div className="window-section-header">
