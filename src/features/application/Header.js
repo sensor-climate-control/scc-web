a73d12a8585f269d2c6a5254d15a0b13000538d9
Daniel Piper <piperdaniel1@gmail.com>
2023-01-24 18:45:46 -0800

    Created account dropdown

--- src/features/application/Header.js
+++ src/features/application/Header.js
@@ -1,18 +1,28 @@
 import './Header.css';
-import HamburgerIcon from '../icons/hamburger';
+import { useState } from 'react';
+import { RxHamburgerMenu } from 'react-icons/rx';
 
 export default function Header(props) {
+    const [showDropdown, setShowDropdown] = useState(false);
+
     return (
         <div className="outer-header-wrapper">
             <div className="inner-header-wrapper">
                 <h2 className="header-brand-name header-text">Liquidity</h2>
                 <h1 className="header-page-name header-text" header-text>{props.page_name}</h1>
 
-                <button className='header-user-dropdown-button' onClick={props.accountCallback}>
+                <button className='header-user-dropdown-button' onClick={() => setShowDropdown(!showDropdown)}>
                     <div className="outer-header-user-dropdown">
-                        <HamburgerIcon class="header-hamburger-icon" />
+                        <RxHamburgerMenu className='header-hamburger-icon' size={43} />
                         <div className="inner-header-user-dropdown">
                             <p className='header-dropdown-first-name header-text'>Welcome back, {props.user_first_name}!</p>
+                        </div>
+
+                        <div className="inner-header-dropdown-items" style={showDropdown ? {display: "block"} : {display: "none"}}>
+                            <p className='header-dropdown-option-text'>Dropdown Item 1</p>
+                            <p className='header-dropdown-option-text'>Dropdown Item 2</p>
+                            <p className='header-dropdown-option-text'>Dropdown Item 3</p>
+                            <p className='header-dropdown-option-text'>Dropdown Item 4</p>
                         </div>
                     </div>
                 </button>
