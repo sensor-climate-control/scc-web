62b36ba2bccd786044b55586311de153bb580bd0
Daniel Piper <piperdaniel1@gmail.com>
2023-01-24 19:05:09 -0800

    toggle prop for newWindow

--- src/features/weather/NewWindow.js
+++ src/features/weather/NewWindow.js
@@ -1,19 +1,29 @@
 import './NewWindow.css'
 import './WindowSection.css'
+import { useState } from 'react';
 import { AiOutlinePlusCircle } from 'react-icons/ai'
 
 export default function NewWindow() {
-    return (
-        <button className="window-section-wrapper">
-            <div className="inner-window-section-wrapper">
-                <div className="plus-sign-icon">
-                    <AiOutlinePlusCircle size={43} />
-                </div>
+    const [toggle, setToggle] = useState(false);
 
-                <div className="window-section-header">
-                    <h1 className="window-section-header-text">Add a new window...</h1>
+    return (
+        <button className="window-section-wrapper" onClick={() => setToggle(!toggle)}>
+            {toggle ? (
+                <div className='toggled-window-section-wrapper'>
+                    <div className='toggled-window-header'>
+                    </div>
                 </div>
-            </div>
+            ):(
+                <div className="inner-window-section-wrapper">
+                    <div className="plus-sign-icon">
+                        <AiOutlinePlusCircle size={43} />
+                    </div>
+
+                    <div className="window-section-header">
+                        <h1 className="window-section-header-text">Add a new window...</h1>
+                    </div>
+                </div>
+            )}
         </button>
     );
 }
