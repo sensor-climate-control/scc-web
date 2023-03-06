37a4999520b9069a4c00c0d3d78db8d77dcd0e81
Daniel Piper <piperdaniel1@gmail.com>
2023-03-06 15:22:08 -0800

    basic rectangle layout

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,6 +2,7 @@ import WindowOverview from "../features/weather/WindowOverview";
 import GraphSection from "../features/weather/graph/GraphSection";
 import './Home.css'
 import { api } from '../reduxApi';
+import LayoutSection from "../features/weather/home_layout/LayoutSection";
 
 const HOME_ID = "63ed9cb48af0fbb8f0201c11";
 
@@ -11,66 +12,9 @@ export default function Home() {
         pollingInterval: 3000,
     });
 
-    // const { home_data, home_error, home_isLoading } = api.useGetWeatherQuery(HOME_ID);
-
-    // console.log(home_data, home_error, home_isLoading)
-
-    // let sensor_details = [];
-    // if (home_data) {
-    //     console.log("Home data: " + home_data)
-    //     for (let i = 0; i < home_data.sensors.length; i++) {
-    //         console.log("Querying sensor " + home_data.sensors[i] + "...")
-    //         sensor_details.push(api.useGetSensorDetailsQuery(HOME_ID, home_data.sensors[i]));
-    //     }
-    //     // get details of each sensor
-    // }
-
-    // console.log(sensor_details)
-
-
-    // if (isLoading) return <div>Loading...</div>
-    // if (error) return <div>Error: {error.message}</div>
-
     // fake data while loading
     let window_data = []
 
-    // if (!data) {
-    //     window_data = [
-    //         {
-    //             name: "Bedroom Window",
-    //             status: "open",
-    //             temp: 72,
-    //             humidity: 50,
-    //         },
-    //         {
-    //             name: "Living Room Window",
-    //             status: "open",
-    //             temp: 72,
-    //             humidity: 50,
-    //         },
-    //         {
-    //             name: "Kitchen Window",
-    //             status: "close_soon",
-    //             temp: 72,
-    //             humidity: 50,
-    //         },
-    //         {
-    //             name: "Guest Bedroom Window",
-    //             status: "closed",
-    //             temp: 72,
-    //             humidity: 50,
-    //         }
-    //     ]
-
-    //     faker.seed(123);
-
-    //     points = [];
-    //     for (let i = 0; i < 100; i++) {
-    //         points.push({
-    //             x: i,
-    //             y: faker.datatype.number({min: 62, max: 74})
-    //         });
-    //     }
     // switch to real data
     if (data) {
         window_data = []
@@ -96,18 +40,32 @@ export default function Home() {
 
             window_data.push(window)
         }
-
-        // use last 100 readings
-        // points = [];
-        // for (let i = data[0].readings.length - 100; i < data[0].readings.length; i++) {
-        //     points.push(data[0].readings[i].temp_f)
-        // }
     }
+
+    let orientations = ["north", "south", "east", "west"]
+
+    let n_orientations = ["north", "north", "north", "north"]
+    let s_orientations = ["south", "south", "south", "south"]
+    let e_orientations = ["east", "east", "east", "east"]
+    let w_orientations = ["west", "west", "west", "west"]
+
+    let map_window_data = window_data.map((window) => {
+        return {
+            window_orientation: s_orientations.pop(),
+            data: window,
+        }
+    })
 
     return (
         <div className="outer-home-sections-wrapper">
-            <WindowOverview windows={window_data} />
-            <GraphSection windows={window_data}/>
+            <div className="top-section-wrapper">
+                <WindowOverview windows={window_data} />
+                <GraphSection windows={window_data}/>
+            </div>
+
+            <div className="bottom-section-wrapper">
+                <LayoutSection data={map_window_data} />
+            </div>
         </div>
     );
 }
