7e4aecbe301bf5c0ca4fa59eb62d29e792f33b18
Daniel Piper <piperdaniel1@gmail.com>
2023-03-06 15:49:29 -0800

    Added labels to WindowBox

--- src/features/weather/home_layout/WindowBox.js
+++ src/features/weather/home_layout/WindowBox.js
@@ -2,9 +2,9 @@ export default function WindowBox(props) {
     return (
         <div className="layout-window-box">
             <h1 className='layout-window-name'>{props.data.name}</h1>
-            <h2 className='layout-window-text'>{props.data.status}</h2>
-            <h2 className='layout-window-text'>{props.data.temp}</h2>
-            <h2 className='layout-window-text'>{props.data.humidity}</h2>
+            <h2 className='layout-window-text'>Status: {props.data.status.charAt(0).toUpperCase() + props.data.status.slice(1)}</h2>
+            <h2 className='layout-window-text'>Temperature: {props.data.temp} F</h2>
+            <h2 className='layout-window-text'>Humidity: {props.data.humidity}%</h2>
         </div>
     )
 }
