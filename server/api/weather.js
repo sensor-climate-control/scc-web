72729db894337973bf67b22271bfef204ff933a5
Ryan Zimmerman <zimmerry@oregonstate.edu>
2023-04-18 09:24:27 -0700

    Setup weather endpoints to use zipcode

--- server/api/weather.js
+++ server/api/weather.js
@@ -1,19 +1,27 @@
 const router = require('express').Router();
-const weather = require('../data/weather.json');
 const { requireAuthentication } = require('../lib/auth');
 const { geoLocation } = require('../lib/geo');
 require('dotenv').config();
 
 const openweathermapApiKey = process.env.OWM_API_KEY
 const airnowApiKey = process.env.AIRNOW_API_KEY
-const zip = "97330"
 
 // Fetches and returns 3-hour, 5-day weather forecast
 // Calls OpenWeatherMap API
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 // Requires authentication
 router.get('/', requireAuthentication, async function (req, res, next) {
-    res.status(200).send(weather.fiveDay)
+    if(req.query && req.query.zip) {
+        const geo = await geoLocation(req.query.zip, "US")
+
+        let fiveDayWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${geo.lat}&lon=${geo.lon}&units=imperial&appid=${openweathermapApiKey}`)
+        fiveDayWeather = await fiveDayWeather.json()
+        res.status(200).send(fiveDayWeather)
+    } else {
+        res.status(400).send({
+            error: "Request does not have a zip code in query string"
+        })
+    }
 })
 
 // Fetches and returns current weather forecast
@@ -21,11 +29,18 @@ router.get('/', requireAuthentication, async function (req, res, next) {
 // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 // Requires authentication
 router.get('/now', requireAuthentication, async function (req, res, next) {
-    const geo = await geoLocation(zip, "US")
-    console.log("==== geo: ", geo)
-    let currentWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${geo.lat}&lon=${geo.lon}&units=imperial&appid=${openweathermapApiKey}`)
-    currentWeather = await currentWeather.json()
-    res.status(200).send(currentWeather)
+    if(req.query && req.query.zip) {
+        const geo = await geoLocation(req.query.zip, "US")
+
+        let currentWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${geo.lat}&lon=${geo.lon}&units=imperial&appid=${openweathermapApiKey}`)
+        currentWeather = await currentWeather.json()
+
+        res.status(200).send(currentWeather)
+    } else {
+        res.status(400).send({
+            error: "Request does not have a zip code in query string"
+        })
+    }
 })
 
 // Fetches and returns AQI forecast
@@ -33,7 +48,16 @@ router.get('/now', requireAuthentication, async function (req, res, next) {
 // https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode={zip}&date=2022-11-09&distance=25&API_KEY={API key}
 // Requires authentication
 router.get('/aqi', requireAuthentication, async function (req, res, next) {
-    res.status(200).send(weather.aqiForecast)
+    if(req.query && req.query.zip) {
+        let response = await fetch(`https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=${req.query.zip}&date=2022-11-09&distance=25&API_KEY=${airnowApiKey}`)
+        response = await response.json()
+
+        res.status(200).send(response)
+    } else {
+        res.status(400).send({
+            error: "Request does not have a zip code in query string"
+        })
+    }
 })
 
 // Fetches and returns current AQI observation
@@ -41,10 +65,16 @@ router.get('/aqi', requireAuthentication, async function (req, res, next) {
 // https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode={zip}&distance=25&API_KEY={API key}
 // Requires authentication
 router.get('/aqi/now', requireAuthentication, async function (req, res, next) {
-    let response = await fetch(`https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=${zip}&distance=25&API_KEY=${airnowApiKey}`)
-    response = await response.json()
-
-    res.status(200).send(response)
+    if(req.query && req.query.zip) {
+        let response = await fetch(`https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=${req.query.zip}&distance=25&API_KEY=${airnowApiKey}`)
+        response = await response.json()
+    
+        res.status(200).send(response)
+    } else {
+        res.status(400).send({
+            error: "Request does not have a zip code in query string"
+        })
+    }
 })
 
 exports.router = router;
