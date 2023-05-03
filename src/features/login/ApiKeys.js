472412e14726f26a6a51f7ca3cf3b9566c010b56
Ryan Zimmerman <53264075+zimmerry@users.noreply.github.com>
2023-05-03 11:57:56 -0700

    Setup editing user information, managing API keys (#80)
    * Setup editing of user info
    
    * Fix linting issues
    
    * Fix styling
    
    * Reorganize user info, add api key creation component
    
    * Fix remove API key endpoint, add remove API key UI component

--- src/features/login/ApiKeys.js
+++ src/features/login/ApiKeys.js
@@ -1,55 +1,19 @@
 import React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-
+import TimestampToDateTime from '../application/TimestampToDateTime';
 import MyCard from "../application/MyCard";
-// import { useGetApiKeysQuery } from '../../reduxApi';
-// import { useStore } from 'react-redux'
-// import Header from '../application/Header';
-// import { useNavigate } from 'react-router-dom';
+import MyTable from '../application/MyTable';
 
 function ApiKeys (props) {
     console.log("==== props: ", props)
 
     const rows = []
     for (let i = 0; i < props.api_keys.length; i++) {
-        rows.push({name: props.api_keys[i].name, token: props.api_keys[i].token, created: props.api_keys[i].created, expires: props.api_keys[i].expires})
+        rows.push([props.api_keys[i].name, props.api_keys[i].token, TimestampToDateTime(props.api_keys[i].created), TimestampToDateTime(props.api_keys[i].expires)])
     }
 
     return (
         <MyCard title="API Keys" style={{ width: '50rem', backgroundColor: '#646c7a' }}>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-                    <TableHead>
-                    <TableRow>
-                        <TableCell>Name</TableCell>
-                        <TableCell>Token</TableCell>
-                        <TableCell>Created</TableCell>
-                        <TableCell>Expires</TableCell>
-                    </TableRow>
-                    </TableHead>
-                    <TableBody>
-                    {rows.map((row) => (
-                        <TableRow
-                        key={row.name}
-                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                        <TableCell component="th" scope="row">
-                            {row.name}
-                        </TableCell>
-                        <TableCell style={{ whiteSpace: "normal", overflowWrap: "anywhere" }}>{row.token}</TableCell>
-                        <TableCell>{row.created}</TableCell>
-                        <TableCell>{row.expires}</TableCell>
-                        </TableRow>
-                    ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+            <MyTable headers={['Name', 'Token', 'Created', 'Expires']} rows={rows} sx={{ minWidth: 700 }}/>
         </MyCard>
     )
 }
