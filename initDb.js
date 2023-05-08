5841cc0114b8426dee223e8911a923417e7e40bb
Ryan Zimmerman <53264075+zimmerry@users.noreply.github.com>
2023-05-08 16:56:35 -0700

    Fix docker config and initialization scripts (#86)

--- initDb.js
+++ initDb.js
@@ -1,12 +1,44 @@
-const { connectToDb, getDbReference, closeDbConnection } = require('./server/lib/mongo')
+const { connectToDb, getDbReference, closeDbConnection } = require('./server/lib/mongo');
+const { insertNewUser } = require('./server/models/users');
+require('dotenv').config();
 
-connectToDb(async function () {
-    const db = getDbReference()
-    await db.createCollection('homes')
-    await db.createCollection('sensors')
-    await db.createCollection('users')
-  
-    closeDbConnection(function () {
-      console.log("== DB connection closed")
-    })
+const adminEmail = process.env.WEB_ADMIN_EMAIL
+const adminPass = process.env.WEB_ADMIN_PASS
+
+connectToDb(async function () {    
+  console.log("== DB connection open")
+
+  const db = getDbReference()
+
+  const collections = await db.listCollections().toArray()
+  let collectionNames = []
+  collections.forEach(collection => {
+    collectionNames.push(collection.name)
+  });
+  console.log("==== collections: ", collectionNames)
+
+  const collectionsToCreate = ['homes', 'sensors', 'users', 'weather']
+
+  collectionsToCreate.forEach(async (collectionToCreate) => {
+    console.log("==== collectionToCreate: ", collectionToCreate)
+    if(collectionNames.includes(collectionToCreate)) {
+      console.log(`'${collectionToCreate}' collection already exists, skipping`)
+    } else {
+      console.log(`Creating '${collectionToCreate}' collection`)
+      await db.createCollection(collectionToCreate)
+    }
   })
+
+  console.log("==== admin userid: ", await insertNewUser({
+    name: "admin",
+    email: adminEmail,
+    password: adminPass,
+    admin: true,
+    homes: [],
+    api_keys: []
+  }))
+
+  closeDbConnection(function () {
+    console.log("== DB connection closed")
+  })
+})
--- server/lib/mongo.js
+++ server/lib/mongo.js
@@ -2,35 +2,37 @@
  * Module for working with a MongoDB connection.
  */
 
-const { MongoClient } = require('mongodb')
+const { MongoClient } = require('mongodb');
 
 require('dotenv').config();
 
 const mongoDbName = process.env.MONGO_DB_NAME || 'prod'
 
 const mongoUrl = (process.env.MONGO_URL) ? (process.env.MONGO_URL) : 
-	(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}`)
+	(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}?directConnection=true`)
 console.log("==== mongoUrl: ", mongoUrl)
 
 let db = null
 let _closeDbConnection = null
-exports.connectToDb = function (callback) {
-  	MongoClient.connect(mongoUrl, function (err, client) {
-    	if (err) {
-      		throw err
-    	}
-    	db = client.db(mongoDbName)
-    	_closeDbConnection = function () {
-      		client.close()
-    	}
-    	callback()
-  	})
+exports.connectToDb = async function (callback) {
+	const client = new MongoClient(mongoUrl)
+	try {
+		await client.connect();
+
+		db = client.db(mongoDbName)
+		_closeDbConnection = async function () {
+			await client.close()
+		}
+		callback()
+	} catch (e) {
+		throw(e)
+	}
 }
 
 exports.getDbReference = function () {
   	return db
 }
 
-exports.closeDbConnection = function (callback) {
-  	_closeDbConnection(callback)
+exports.closeDbConnection = async function (callback) {
+  	await _closeDbConnection(callback)
 }
