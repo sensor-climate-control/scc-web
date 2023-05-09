69d70feb51d2a9c801fe18021b705a95c0145886
Ryan Zimmerman <zimmerry@oregonstate.edu>
2023-05-09 16:41:06 -0700

    Wrap main in connectToSMTP

--- server/lib/mail.js
+++ server/lib/mail.js
@@ -1,13 +1,10 @@
 const nodemailer = require("nodemailer")
 require('dotenv').config();
 
-async function sendMail(to, subject, text) {
-    console.log("==== process.env.SMTP_HOST: ", process.env.SMTP_HOST)
-    console.log("==== process.env.SMTP_PORT: ", process.env.SMTP_PORT)
-    console.log("==== process.env.SMTP_USER: ", process.env.SMTP_USER)
-    console.log("==== process.env.SMTP_PASS: ", process.env.SMTP_PASS)
-    console.log("==== process.env.SMTP_FROM: ", process.env.SMTP_FROM)
-    let transporter = nodemailer.createTransport({
+let transporter = null
+
+async function connectToSMTP(callback) {
+    transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
         secure: false,
@@ -25,10 +22,15 @@ async function sendMail(to, subject, text) {
         if (error) {
           console.log(error);
         } else {
-          console.log("Server is ready to take our messages");
+          console.log("SMTP server is ready to take our messages");
         }
     });
 
+    callback()
+}
+exports.connectToSMTP = connectToSMTP
+
+async function sendMail(to, subject, text) {
     let info = await transporter.sendMail({
         from: process.env.SMTP_FROM,
         to: to,
